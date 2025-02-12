045870b0e6a29bb342790b96322a8c8891be612c
Gary Tyree <gatyree1@gmail.com>
2025-02-12 15:42:37 -0800

    Separated Style attributes and updated style of home page

--- views.py
+++ views.py
@@ -11,7 +11,7 @@ views = Blueprint(__name__,"views")
 
 @views.route("/")
 def home():
-    return render_template("index2.html")
+    return render_template("index.html")
 
 @views.route("/Resume")
 def resume():
